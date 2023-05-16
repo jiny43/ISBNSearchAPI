0316dc01c0c98293c2e301462a12899b2969b580
jieun <jiny273588@gmail.com>
2023-05-16 17:28:10 +0900

    fetch api

--- API.js
+++ API.js
@@ -1,8 +1,18 @@
 
 const https = require('http');
 
-const ISBNUrl=`https://www.nl.go.kr/NL/search/openApi/search.do?key=1c81199b2a9a2305bc71bb993c45e032794d810adae4c92544bd3d8ac9c2a574&kwd=${search}`;
 
-const server = https.createServer(function(req,res){
-if(req.method ==='GET' && req.url ==='/')
-)}
+const ISBNUrl=`https://www.nl.go.kr/NL/search/openApi/search.do?key=1c81199b2a9a2305bc71bb993c45e032794d810adae4c92544bd3d8ac9c2a574&kwd=${data}`;
+
+
+// const server = https.createServer(function(req,res){
+// if(req.method ==='GET' && req.url ==='/'){
+//   res.writeHead(200,{'Content-Type':'text/html'});
+//   res.write();
+//   res.end();
+// }
+// )});
+
+
+
+// server.listen(2080);
